fa5cb95fdbd0e3f966d6d1f573b1c57e22d2066b
nikitaboal <nikbold-02@yandex.ru>
2022-05-22 23:49:28 +0300

    v1.2

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,16 +1,11 @@
 // Copyright 2021 Your Name <your_email>
 
 #include <stdexcept>
-#include <thread>
 
 #include <gtest/gtest.h>
 
-#include <sertificate.hpp>
+#include <parser.hpp>
 
 TEST(Example, EmptyTest) {
-    EXPECT_THROW(example(), std::runtime_error);
-}
-
-TEST(DISABLED_Snapshot, Speen) {
-  for (;;) std::this_thread::yield();
+  EXPECT_THROW(example(), std::runtime_error);
 }
